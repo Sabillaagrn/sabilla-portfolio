697c4ee231fce0e91b70481ae66a79670401c696
Sabilla Anggraeni <sabillabilaa14@gmail.com>
2026-01-05 16:24:29 +0700

    second commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: "STUDEE",
     desc: "Aplikasi manage efectivitas study and documentation progress task",
-    image: "/projects/STUDEE.png",
+    image: "/projects/studee.png",
     tags: ["Laravel", "Tailwind", "SQLite"],
     figma: "",
     github: "https://github.com/Sabillaagrn/STUDEE.git"
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: "Snappie-Photobooth",
     desc: "Photobooth website, capture / upload photo with frame custom and filter",
-    image: "/projects/Snappie.png",
+    image: "/projects/snappie.png",
     tags: ["React", "Tailwind", "Supabase"],
     figma: "https://www.figma.com/design/Kh4WcJcvnok2dG0hbY6i6x/Snappie?node-id=1-8&t=zQom87a7pbB0RLbu-1",
     github: "https://github.com/Sabillaagrn/photobooth-snappie.git"
@@ -19,7 +19,7 @@ export const projects = [
   {
     title: "BillUp",
     desc: "Split bill App, scan ur bill and shrae tagihan for your friends",
-    image: "/projects/BillUp.png",
+    image: "/projects/billUp.png",
     tags: ["Kotlin", "API", "Android Studio"],
     figma: "https://www.figma.com/design/T5vHUMBwTP0wepSuQ4HEij/BillUp--IMK-?node-id=0-1&t=uaoCXQ5bGa6lvBWT-1",
     github: "https://github.com/Sabillaagrn/BillUp.git"
@@ -27,7 +27,7 @@ export const projects = [
   {
     title: "Sawala Edu",
     desc: "Real-time weather data with beautiful UI",
-    image: "/projects/SawalaEdu.png",
+    image: "/projects/sawalaEdu.png",
     tags: ["React", "Tailwind", "API", "Charts"],
     figma: "https://www.figma.com/design/czdaxlaX1XHxGmQiTo4uVw/SawalaEdu?node-id=2-2&t=TzEsJCNNktq7uxmM-1",
     github: "https://github.com/Sabillaagrn/Sawala_Edu.git"
@@ -36,7 +36,7 @@ export const projects = [
   {
     title: "SOCA",
     desc: "An application for detecting nearby objects to assist the mobility of visually impaired individuals.",
-    image: "/projects/SOCA.png",
+    image: "/projects/soca.png",
     tags: ["YOLOv8", "Java", "Android Studio"],
     figma: "https://www.figma.com/design/TbHbT3sj2aU6H1j0VEglaI/TBO?node-id=0-1&t=mtpN57irAOLqAKCM-1",
     github: "https://github.com/Sabillaagrn/SOCA.git"
